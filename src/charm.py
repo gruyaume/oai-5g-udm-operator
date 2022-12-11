9b7f6e274bed025dcaab1825850085f658db18d0
guillaume <guillaume.belanger27@gmail.com>
2022-12-11 14:05:12 -0500

    Handles modelerror

--- src/charm.py
+++ src/charm.py
@@ -17,7 +17,7 @@ from charms.observability_libs.v1.kubernetes_service_patch import (  # type: ign
 from jinja2 import Environment, FileSystemLoader
 from ops.charm import CharmBase, ConfigChangedEvent
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, ModelError, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
@@ -84,7 +84,11 @@ class Oai5GUDMOperatorCharm(CharmBase):
     def _udm_service_started(self) -> bool:
         if not self._container.can_connect():
             return False
-        if not self._container.get_service(self._service_name).is_running():
+        try:
+            service = self._container.get_service(self._service_name)
+        except ModelError:
+            return False
+        if not service.is_running():
             return False
         return True
 
