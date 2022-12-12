9d906ba2c21994a9fe1564921a08e17dc7e92cb1
guillaume <guillaume.belanger27@gmail.com>
2022-12-12 09:16:12 -0500

    Simplifies charm configuration

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -83,8 +83,6 @@ class TestCharm(unittest.TestCase):
             udr_fqdn,
         ) = self._create_udr_relation_with_valid_data()
 
-        self.harness.update_config(key_values={"sbiIfName": "eth0"})
-
         mock_push.assert_called_with(
             path="/openair-udm/etc/udm.conf",
             source="## UDM configuration file\n"
@@ -135,8 +133,6 @@ class TestCharm(unittest.TestCase):
         self.harness.set_can_connect(container="udm", val=True)
         self._create_nrf_relation_with_valid_data()
         self._create_udr_relation_with_valid_data()
-
-        self.harness.update_config({"sbiIfName": "eth0"})
 
         expected_plan = {
             "services": {
